4f412b895b03d529a2c5800c8250082ec1fd1559
kapi <kapihl@gmail.com>
2017-03-19 15:20:32 +0100

    add button, <li> and re-insert in DOM

--- legestue/track.js
+++ legestue/track.js
@@ -11,7 +11,35 @@ console.log("Document has " + item.length + " items")
 console.log("First " + elems + " are: ")
 
 for (var i = 0; i < elems; i++){
+  console.log("Before alterations")
   console.log(item[i])
+
+  // add a button
+  var but_id = "insert_button_id" + i
+  var new_but = document.createElement("BUTTON")
+  new_but.id = but_id
+  var but_text = document.createTextNode("TRACK" + i)
+  new_but.appendChild(but_text)
+  item[i].appendChild(new_but)
+
+  // wrap in <li>, add ID
+  var wrapper = document.createElement("LI")
+  wrapper.appendChild(item[i])
+  var wrap_id = "insert_wrapper_id"+ i
+  wrapper.id = wrap_id
+
+  // insert back into html
+  var parent = item[i].parentElement
+  parent.appendChild(wrapper)
+
+  // fetch recent created elements and log
+  var load_button = document.getElementById(but_id)
+  console.log("After id and button added")
+  console.log(load_button)
+
+  var load_wrapper = document.getElementById(wrap_id)
+  console.log(load_wrapper)
+  
 }
 
 console.log("Printing elements done!")
