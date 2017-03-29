f2d399274ea5a8755041958946012b60a3b0e20f
kapi <kapihl@gmail.com>
2017-03-29 20:17:08 +0200

    rename: from remove_duplicates() to ignore_same_links(), add add_buttons(), re-order code: first functions then 'main', del debug msgs

--- legestue/track.js
+++ legestue/track.js
@@ -1,22 +1,11 @@
 // track.js: simple recording of clics on a webpage. HTML file: downloaded file from TMC
 
-var item = document.getElementsByTagName("a")
-var dict = {}
-
-line_sz = 160
-elems   = 330
-mark = Array(line_sz).join("#") + "\n"
-
-console.log(mark)
-console.log("Document has " + item.length + " items")
-console.log("First " + elems + " are: ")
-
-
-function remove_dublicates(){
+// ensure that links do not repeat
+function ignore_same_links(){
   // iterate trough dom-list, make dictionary of href = full dom item
   // before inserting, do lookup. Insert only if not found
 
-  for (var i = 0; i < elems; i++){
+  for (var i = 0; i < item.length; i++){
     var my_href = item[i].href
     var key = "".concat(my_href)
     console.log("HREF:  " + my_href)
@@ -28,59 +17,68 @@ function remove_dublicates(){
   }
 }
 
-// do removal
-var some = remove_dublicates()
+// add buttons to links held in dict.
+function add_buttons(){
+  var i = 0
+  for (var key in dict){
+    i++
+    console.log("Before alterations")
+    console.log(dict[key])
+  //  console.log(testf(dict[key]))
+    var current = dict[key]
+    var parent = current.parentElement
+    // add a button
+    var but_id = "insert_button_id" + i
+    var new_but = document.createElement("BUTTON")
+    new_but.id = but_id
+    var but_text = document.createTextNode("TRACK" + i)
+    new_but.appendChild(but_text)
+    // add 2 LI
+    var li1 = document.createElement("LI")
+    var li2 = document.createElement("LI")
+    // wrap in UL
+    var ul = document.createElement("UL")
+    li1.appendChild(current)
+    li2.appendChild(new_but)
+    ul.appendChild(li1)
+    ul.appendChild(li2)
+    var ul_id = "insert_ul_id"+ i
+    ul.id = ul_id
+    // FIX: both button and link should each be LI child in new UL. UL -> DOM insert
 
-console.log("len of item " + item.length)
-console.log("len of dict " + Object.keys(dict).length)
+    // insert back into html
+    
+    console.log("parent is: "  + parent)
+    console.log("wrapper is: " + ul)
+    console.log("current is: " + current)
+    parent.appendChild(ul)
 
-// counter
-var i = 0
-for (var key in dict){
-  i++
-  console.log("Before alterations")
-  console.log(dict[key])
-//  console.log(testf(dict[key]))
-  var current = dict[key]
-  var parent = current.parentElement
-  // add a button
-  var but_id = "insert_button_id" + i
-  var new_but = document.createElement("BUTTON")
-  new_but.id = but_id
-  var but_text = document.createTextNode("TRACK" + i)
-  new_but.appendChild(but_text)
-  // add 2 LI
-  var li1 = document.createElement("LI")
-  var li2 = document.createElement("LI")
-  // wrap in UL
-  var ul = document.createElement("UL")
-  li1.appendChild(current)
-  li2.appendChild(new_but)
-  ul.appendChild(li1)
-  ul.appendChild(li2)
-  var ul_id = "insert_ul_id"+ i
-  ul.id = ul_id
-  // FIX: both button and link should each be LI child in new UL. UL -> DOM insert
+    // fetch recent created elements and log
+    var load_button = document.getElementById(but_id)
+    console.log("After id and button added")
+    console.log(load_button)
 
-  // insert back into html
-  
-  console.log("parent is: "  + parent)
-  console.log("wrapper is: " + ul)
-  console.log("current is: " + current)
-  parent.appendChild(ul)
+    var load_wrapper = document.getElementById(ul_id)
+    console.log(load_wrapper)
+    }
+}
 
-  // fetch recent created elements and log
-  var load_button = document.getElementById(but_id)
-  console.log("After id and button added")
-  console.log(load_button)
+// initially: read all links
+var item = document.getElementsByTagName("a")
+var dict = {}
 
-  var load_wrapper = document.getElementById(ul_id)
-  console.log(load_wrapper)
-  }
+line_sz = 120
+mark = Array(line_sz).join("#") + "\n"
 
-console.log("Printing elements done!")
-console.log("dict had " + i + " elems")
 console.log(mark)
+console.log("Document has " + item.length + " items")
 
+// remove duplicates and add buttons
+ignore_same_links()
+add_buttons()
+
+console.log("len of dict " + Object.keys(dict).length)
+console.log("Printing elements done!")
+console.log(mark)
 
 
