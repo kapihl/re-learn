7ccbe68d826e664d5b86bed3af9f785001fe5f03
kapi <kapihl@gmail.com>
2017-06-05 15:56:18 +0200

    Response helloworld or buttons html. Invoke: localhost:3000/{hello|buttons}

---
+++ myapp/serveMultiReqs.js
@@ -0,0 +1,44 @@
+// prereq: html file on local machine, in variable localFile.
+// invoke: localhost:3000/{hello|buttons}
+
+var localFile = "simplePage.html";
+const express = require('express');
+const app = express();
+var request = require('request');
+var fs = require('fs');
+
+app.listen(3000, function () {
+  console.log('Port 3000!');
+  console.log('Two params: hello => hello world, buttons => html w. buttons');
+});
+
+// read page from file
+app.get('/buttons', function (req, res){
+  readHtml(function(data){
+    console.log("Page :\n" + data);
+    res.setHeader('Content-Type', 'text/html');
+    res.send(data); 
+  });
+});
+
+// build simple html file inline
+app.get('/hello', function (req, res){
+  console.log("simple page");
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>');
+  res.write('<body>');
+  res.write('<h1>Hello, World - go node.js!</h1>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
+
+// load file from local machine, app folder
+function readHtml(cb){
+  var result = "";
+  fs.readFile(localFile, function (err, data) {
+    if (err) throw err;
+    console.log("page read");
+    cb(data);
+  });
+}
